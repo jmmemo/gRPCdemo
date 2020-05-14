a2b9b8aaa9657efc38ee759ea708056fa3cb217b
KIRI <1439420767@qq.com>
2020-05-14 11:41:44 +0800

    gRPCdemoV1

---
+++ client.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	pb "grpc/proto"
+)
+
+func main() {
+	conn, err := grpc.Dial("127.0.0.1:8080", grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("连接异常", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewUserInfoServiceClient(conn)
+	req := new(pb.UserRequest)
+	req.Name = "zs"
+
+	response, err := client.GetUserInfo(context.Background(), req)
+	if err != nil {
+		fmt.Println("响应异常", err)
+	}
+	fmt.Println("响应结果", response)
+}
---
+++ server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	pb "grpc/proto"
+	"net"
+)
+
+type UserInfoService struct {
+}
+
+var u = UserInfoService{}
+
+func (s *UserInfoService) GetUserInfo(ctx context.Context, req *pb.UserRequest) (resp *pb.UserResponse, err error) {
+	name := req.Name
+
+	if name == "zs" {
+		resp = &pb.UserResponse{
+			Id:    1,
+			Name:  name,
+			Age:   22,
+			Hobby: []string{"Sing", "Run"},
+		}
+	}
+	return
+}
+func main() {
+	addr := "127.0.0.1:8080"
+	listen, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Println("监听异常", err)
+	}
+	fmt.Println("监听端口", addr)
+	server := grpc.NewServer()
+	pb.RegisterUserInfoServiceServer(server, &u)
+	server.Serve(listen)
+}
